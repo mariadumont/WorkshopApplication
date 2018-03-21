240b3927f37866bf319e345b7733d8092c044101
alu20482156n <alu20482156n@ti1-02>
2018-03-21 19:32:45 +0100

    Create WorkshopApplication class

---
+++ src/WorkshopApplication.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class WorkshopApplication {
+
+	// Product information
+	private int codProd;
+	private String prodDescription;
+	private double prodPrice;
+	private int prodStock;
+	
+	// Client information
+	private int codCli;
+	private String cliDNI;
+	private String cliName;
+	private String cliFirstName;
+	
+	// Invoice information
+	private int codInvoice;
+	private Date dateInvoice;
+	private double sum;
+	private int discount;
+	
+	
+	// Invoice line information
+	private int numInvoiceLine;
+	private int numArticles;
+	private int codArt;
+	private int codQuantity;
+	
+	
+}
